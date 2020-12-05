80fee253da61505eda527dd8ee63fe151d2e0d1a
CoolMan42 <zachedler88@gmail.com>
2020-12-05 12:40:22 -0500

    commit

--- config.h
+++ config.h
@@ -3,10 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 50;
-static const unsigned int gappiv    = 50;
-static const unsigned int gappoh    = 50;
-static const unsigned int gappov    = 50;
+static const unsigned int gappx     = 5;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
